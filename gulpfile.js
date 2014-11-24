3bb0c2a7c84f28cc986cf4cb2856731325b6c2b9
skilbjo <john.skilbeck@gmail.com>
2014-11-24 13:32:45 -0800

    refactored gulp file, new content on landing page

--- gulpfile.js
+++ gulpfile.js
@@ -7,45 +7,52 @@ var gulp 						= require('gulp')
 	, rename 					= require('gulp-rename')
 	, livereload 			= require('gulp-livereload')
 	, spawn    				= require('child_process').spawn
-	, publicDir 			= './public/src/'
-	, appDir 					= './app/'
-	, node
-	, jsLocations   = ['server.js'
-		, appDir + '**/*.js'
-		, appDir + '*.js'
-		, publicDir + 'js/**/*.js']
-	, jsCommon = [ publicDir + 'js/analytics/*.js'
-		, publicDir + 'js/template/*.js']
-	, jsPurchase = ['bower_components/jquery.payment/lib/jquery.payment.js'
-		, publicDir + 'js/purchase/*.js']
-  , cssLocations  = [ publicDir + 'css/*.css']
-  , jadeLocations = [ appDir + 'view/**/*.jade'
-  	, appDir + 'view/**/elements/*.jade'];
+	, pub 			 			= './public/src/'
+	, root	 					= './app/'
+	, bower 					= 'bower_components/';
+
+var src = {
+	js: {
+		all: [root + '**/*.js', root + '*.js', pub + 'js/**/*.js','server.js'],
+		pub: [pub + 'js/analytics/*.js', pub + 'js/template/*.js'],
+		pur: [pub + '/js/purchase/*.js', bower + 'jquery.payment/lib/jquery.payment.js']
+	},
+	css: {
+		all: [pub + 'css/*.css']
+	},
+	jade: {
+		all: [root + 'view/**/*.jade', root + 'view/**/elements/*.jade']
+	}
+};
 
 gulp.task('default',
- ['lint','css','js','server','watch']
+ ['build','server','watch']
+);
+
+gulp.task('build',
+	['lint','css','js','jade']
 );
 
 gulp.task('js',
- ['commonjs','purchasejs']
+ ['js.pub','js.pur']
 );
 
 gulp.task('lint', function() {
-	return gulp.src(jsLocations)
+	return gulp.src(src.js.all)
 		.pipe(jshint({laxcomma:true}))
 		.pipe(jshint.reporter(stylish));
 });
 
-gulp.task('commonjs', function() {
-	return gulp.src(jsCommon)
+gulp.task('js.pub', function() {
+	return gulp.src(src.js.pub)
 		.pipe(uglify())
 		.pipe(concat('xl.js'))
 		.pipe(rename({suffix: '.min'}))
 		.pipe(gulp.dest('public/dist'));
 });
 
-gulp.task('purchasejs', function() {
-	return gulp.src(jsPurchase)
+gulp.task('js.pur', function() {
+	return gulp.src(src.js.pur)
 		.pipe(uglify())
 		.pipe(concat('purchase.js'))
 		.pipe(rename({suffix: '.min'}))
@@ -53,20 +60,32 @@ gulp.task('purchasejs', function() {
 });
 
 gulp.task('css', function() {
-	gulp.src(cssLocations)
+	gulp.src(src.css.all)
 		.pipe(minifyCSS({keepBreaks:false}))
 		.pipe(concat('style.css'))
 		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('public/dist'))
-		.pipe(livereload({auto: false}));
+		.pipe(gulp.dest('public/dist'));
 });
+
+gulp.task('jade', function() {
+	gulp.src(src.jade.all);
+});
+
+/* 
+make sure to enable LiveReload JS in the browser
+<script src="http://127.0.0.1:35729/livereload.js?ext=Chrome&amp;extver=2.0.9"></script> 
+*/
 
 gulp.task('watch', function() {
 	livereload.listen();
-	gulp.watch(cssLocations, ['css']);
+	gulp.watch(src.css.all, ['css']).on('change', livereload.changed);
+	gulp.watch(src.js.all, ['js']).on('change', livereload.changed);
+	gulp.watch(src.jade.all, ['jade']).on('change', livereload.changed);
+	console.log('reloaded');
 });
 
 gulp.task('server', function() {
+	var node;
   if (node) node.kill();
   node = spawn('nf', ['start','-x','8080','-e','lib/env/dev.env'], {stdio: 'inherit'});
   node.on('close', function (code) {
